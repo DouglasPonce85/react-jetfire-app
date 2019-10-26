79227dbdd9ddda36e3bd9ac13d3de4b9f1936e49
Douglas Rodriguez <dougdeveloper85@gmail.com>
2019-10-26 17:03:40 -0600

    Fixing navBar bootstrap display

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    const activeStyle = { color: '#F15B2A' };
+    const activeStyle = { color: '#F75952' };
 
     return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <button className="navbar-toggler collapsed" type="button" data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+            </button>
+
             <div className="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
